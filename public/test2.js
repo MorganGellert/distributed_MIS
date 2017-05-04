d0c2cf04dc8a3bc88a4ac7732ee250c8e6902cb5
Alex Tong <alexanderytong@gmail.com>
2017-05-04 13:37:18 -0400

    first working commit

---
+++ public/test2.js
@@ -0,0 +1,11 @@
+//var greuler = require('greuler')
+var instance = greuler({
+  target: '#random',
+  width: 480,
+  height: 300,
+  data: greuler.Graph.random({
+    order: 5,
+    size: 5,
+    connected: true
+  })
+}).update()
